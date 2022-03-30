07fb69db5d49d4f86b9b4b8577652ba3b0a1aa28
Michael Barrientos <mike@phc.health>
2022-03-29 19:16:53 -0700

    Support nested fields, path segments
    Closes #28
    
    Adds support for 2 related cases in URLs:
    * Support path segments like in https://google.aip.dev/127, where the
      URL contains a pattern like `post: "/v1/{parent=publishers/*}/books"`
    * Support nested field names in the URL, where the URL is structured
      like:
      ```
        option (google.api.http) = {
          patch: "/v3/{intent.name=projects/*/locations/*/agents/*/intents/*}"
          body: "intent"
        };
      ```
      This gets translated to `/v3/${req["intent"]["name"]}`
    
    While here, use the newer protoc-gen-go-grpc plugin for generating
    server test code, due to deprecated usage of plugin=grpc (see
    https://github.com/protocolbuffers/protobuf-go/releases/tag/v1.20.0#v1.20-grpc-support)

--- integration_tests/scripts/gen-server-proto.sh
+++ integration_tests/scripts/gen-server-proto.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # remove binaries to ensure that binaries present in tools.go are installed
-rm -f $GOBIN/protoc-gen-go $GOBIN/protoc-gen-grpc-gateway $GOBIN/protoc-gen-swagger
+rm -f $GOBIN/protoc-gen-go $GOBIN/protoc-gen-go-grpc $GOBIN/protoc-gen-grpc-gateway $GOBIN/protoc-gen-swagger
 
 go install \
-	github.com/golang/protobuf/protoc-gen-go \
+	google.golang.org/protobuf/cmd/protoc-gen-go \
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc \
 	github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway \
 	github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
 
-protoc -I . -I ../.. --go_out ./ --go_opt plugins=grpc --go_opt paths=source_relative \
+protoc -I . -I ../.. --go_out ./ --go-grpc_out ./ --go-grpc_opt paths=source_relative \
 	--grpc-gateway_out ./ --grpc-gateway_opt logtostderr=true \
 	--grpc-gateway_opt paths=source_relative \
 	--grpc-gateway_opt generate_unbound_methods=true \
